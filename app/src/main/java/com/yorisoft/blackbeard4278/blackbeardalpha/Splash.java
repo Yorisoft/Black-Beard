10e9c470a769a1a732112d6f92e2aa33fbf0931a
Jesse <yelsinsepulveda@gmail.com>
2017-04-29 18:34:15 -0400

    Splash screen the right way" - geek ranch something something.

--- app/src/main/java/com/yorisoft/blackbeard4278/blackbeardalpha/Splash.java
+++ app/src/main/java/com/yorisoft/blackbeard4278/blackbeardalpha/Splash.java
@@ -9,27 +9,11 @@ public class Splash extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash);
-        Thread splashScreen = new Thread() {
-            public void run () {
-                try {
-                    sleep(4000);
-                } catch (InterruptedException y){
-                    y.printStackTrace();
-                } finally {
-                    Intent mainActivity = new Intent("com.yorisoft.blackbeard4278.blackbeard.MAINACTIVITY");
-                    startActivity(mainActivity);
 
-                }
-            }
-        };
-        splashScreen.start();
-    }
-    @Override
-    protected void onPause (){
-        super.onPause();
-        finish();
-    }
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                finish();
 
+    }
 
 }
